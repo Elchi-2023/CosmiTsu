8b48368d1b5020cfc20233a801a6f22dda914517
ElChi <136360907+IDk-2023@users.noreply.github.com>
2025-04-13 00:33:17 +0300

    Update start-unix.sh

--- start-unix.sh
+++ start-unix.sh
@@ -5,7 +5,8 @@ set -euxo pipefail
 while [ true ]
 do
 	echo "Starting up server..."
-	./venv/bin/python3 start_server.py
+ 	sudo iptables -A INPUT -p tcp --dport 27016 -j ACCEPT
+	./venv/bin/python3 start_server.py &
 	echo "Server has shut down. Will restart in 2 seconds (use CTRL-C to cancel)"
 	sleep 2
 done
