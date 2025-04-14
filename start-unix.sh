1758a15f58916efe09b4193bedc30bdbe15a7c12
ElChi <136360907+IDk-2023@users.noreply.github.com>
2025-04-14 17:02:25 +0300

    Update start-unix.sh

--- start-unix.sh
+++ start-unix.sh
@@ -6,7 +6,7 @@ while [ true ]
 do
 	echo "Starting up server..."
  	sudo iptables -A INPUT -p tcp --dport 27016 -j ACCEPT
-	./venv/bin/python3 start_server.py &
+	./venv/bin/python3 start_server.py
 	echo "Server has shut down. Will restart in 2 seconds (use CTRL-C to cancel)"
 	sleep 2
 done
